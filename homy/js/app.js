868db49a727461c2ac66ac02dc2150b107decd7c
MJulia111 <julia.m241992@gmail.com>
2020-12-10 23:00:53 +0200

    finished desktop version of footer

--- homy/js/app.js
+++ homy/js/app.js
@@ -3,13 +3,13 @@ $(window).resize(function () {
 });
 function adaptive_header(w) {
 	let headerColumn_0 = $('.info-header__column').eq(0),
-		boxContacts = $('.box-contacts'),
+		boxContacts = $('.contacts__box_header'),
 		headerColumn_3 = $('.info-header__column').eq(3),
-		contactsPhone = $('.box-contacts__phone'),
-		chatSocial = $('.social-contacts__chat'),
-		skypeSocial = $('.social-contacts__skype'),
-		contactsEmail = $('.box-contacts__email'),
-		socialContacts = $('.social-contacts');
+		contactsPhone = $('.box-contacts__phone_header'),
+		chatSocial = $('.social-contacts__chat_header'),
+		skypeSocial = $('.social-contacts__skype_header'),
+		contactsEmail = $('.box-contacts__email_header'),
+		socialContacts = $('.social-contacts_header');
 
 	if (w < 700.98) {
 		if ((contactsPhone, chatSocial, skypeSocial, contactsEmail).filter('done').length == 0) {
